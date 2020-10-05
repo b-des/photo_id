9dca14e3f75cdc895c8555d118596bdf8b16c6d2
b-des <alltelex@gmail.com>
2020-10-05 19:12:14 +0300

    Add prod check

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ SHAPE_PREDICTOR_FILE_PATH = ROOT_DIR + "/data/shape_predictor_68_face_landmarks.
 TMP_IMAGE_PATH = '/tmp/photo-id/{}'
 HANDLER_URL = os.environ['HANDLER_URL'] if 'HANDLER_URL' in os.environ else 'handler.php'
 REMOVE_BG_API_KEY = os.environ['REMOVE_BG_API_KEY'] if 'REMOVE_BG_API_KEY' in os.environ else None
-IS_PROD = True if 'environment' in os.environ and os.environ['environment'] == 'prod' else None
+IS_PROD = True if 'environment' in os.environ and os.environ['environment'] == 'prod' else False
 
 ORIGINAL_PHOTO_NAME = 'original'
 NO_BG_PHOTO_NAME = 'original-without-bg'
