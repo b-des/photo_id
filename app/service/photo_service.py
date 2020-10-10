cd0f3b3a8f6e7622f28dfcd7378176272b0f2a19
b-des <alltelex@gmail.com>
2020-10-10 12:17:36 +0300

    Updated logging

--- app/service/photo_service.py
+++ app/service/photo_service.py
@@ -396,7 +396,7 @@ class PhotoService:
     @staticmethod
     def __draw_corner_triangle__(image, corner_position, scale=1):
         if corner_position != "none":
-            logger.debug("Draw triangle. Image width: {}".format(image.size[0]))
+            logger.info("Draw triangle. Image width: %s", image.size[0])
             corner_size = int(image.size[0] / 2)
             rotation_angles = {
                 "TL": {
