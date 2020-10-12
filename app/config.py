f375995d8ebe8bada0a04906dd5da75fb85b4cda
b-des <alltelex@gmail.com>
2020-10-12 19:46:08 +0300

    Changed scale

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FACE_CASCADE_FILE_PATH = ROOT_DIR + "/data/haarcascade_frontalface_default.xml"
 SHAPE_PREDICTOR_FILE_PATH = ROOT_DIR + "/data/shape_predictor_68_face_landmarks.dat"
 TMP_IMAGE_PATH = '/tmp/photo-id/{}'
-LOGGING_FILE = '/tmp/photo-id/logs/logs.log'
+LOGGING_FILE = './tmp/photo-id/logs/logs.log'
 HANDLER_URL = os.environ['HANDLER_URL'] if 'HANDLER_URL' in os.environ else 'handler.php'
 REMOVE_BG_API_KEY = os.environ['REMOVE_BG_API_KEY'] if 'REMOVE_BG_API_KEY' in os.environ else None
 IS_PROD = True if 'environment' in os.environ and os.environ['environment'] == 'prod' else False
