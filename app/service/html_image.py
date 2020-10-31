0b948a5a051911aac4961df408ed390baec11984
Andrii Protsenko <andrii.protsenko@masterofcode.com>
2020-10-31 17:55:24 +0200

    Updated aing collage

--- app/service/html_image.py
+++ app/service/html_image.py
@@ -17,8 +17,8 @@ options = {
 
 def create_collage(url, uid, host):
     url = 'https://pechat.photo/index.php?route=photoid/photoid/collag123e'
-    path = get_tmp_file_path('1234', 'out.jpg')
+    file_name = '{}.{}'.format(config.RESULT_COLLAGE_NAME, config.DEFAULT_PHOTO_EXT)
+    path = get_tmp_file_path(uid, file_name)
     print(path)
     imgkit.from_url(url, path, options=options)
-    file_name = '{}.{}'.format(config.RESULT_PHOTO_NAME, config.DEFAULT_PHOTO_EXT)
     send_file_over_http(host=host, file_path=path, uid=uid, photo_name=file_name, remove_tmp_path=False)
