bc23c0b5e162ffd284b2423c19500c39c57ebe15
Andrii Protsenko <andrii.protsenko@masterofcode.com>
2020-11-02 17:52:26 +0200

    Add dimensions for collage render

--- app/service/html_image.py
+++ app/service/html_image.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(config.LOGGER_NAME)
 
 
 def create_collage(uid, host, dimensions):
-    url = config.COLLAGE_TEMPLATE_URL.format(host, uid, base64.b64encode(dimensions))
+    url = config.COLLAGE_TEMPLATE_URL.format(host, uid, base64.b64encode(str(dimensions).encode('utf-8')))
     logger.info("Generate collage, dimensions: %s", dimensions)
     file_name = '{}.{}'.format(config.RESULT_COLLAGE_NAME, config.DEFAULT_PHOTO_EXT)
     path = get_tmp_file_path(uid, file_name)
